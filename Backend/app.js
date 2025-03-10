cb8bc4d7fdbc2aad3107708901f32b458823a21f
Muhammad Ammar Akbar <ammarakbar4600@gmail.com>
2025-03-10 15:11:02 +0500

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Allow CORS for all origins
 app.use(cors({
-    origin: "http://localhost:5173", // Allows all origins
+    origin: "https://devinproject.vercel.app", // Allows all origins
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
@@ -22,7 +22,7 @@ connectDB()
 
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Origin", "https://devinproject.vercel.app");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     
